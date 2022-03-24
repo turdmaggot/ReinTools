4161cd736110de2cd9de80f49b2ffaea84f5aa57
Reiner Tupaz <rstupaz@hotmail.com>
2022-03-24 15:44:05 +0800

    Added timestamp property

--- MiscTools/Objects/LogEventRecord.cs
+++ MiscTools/Objects/LogEventRecord.cs
@@ -4,6 +4,7 @@ namespace MiscTools.Objects
     public class LogEventRecord
     {
         public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
         public LogEvent Log { get; set; }
     }
 }
